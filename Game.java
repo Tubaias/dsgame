fbf505062c7fee7c10801828e98cea46d44d8756
Tubaias <topias_t@hotmail.com>
2020-12-06 00:32:31 +0200

    circle topology test

--- Game.java
+++ Game.java
@@ -1,98 +1,115 @@
 import java.net.*;
 import java.io.*;
+import java.util.ArrayDeque;
 import java.util.Scanner;
 
 public class Game {
-    private Scanner input;
-    ServerSocket serverSocket;
+    private Scanner keyboard;
     private final int PORT = 25565;
 
+    private ServerSocket serverSocket;
+    private Socket rightSocket;
+    private Socket leftSocket;
+    private SocketHandler rightHandler;
+    private SocketHandler leftHandler;
+
+    private ArrayDeque<String> rightIn;
+    private ArrayDeque<String> rightOut;
+    private ArrayDeque<String> leftIn;
+    private ArrayDeque<String> leftOut;
+
     public Game(Scanner input) {
-        this.input = input;
+        this.keyboard = input;
+        rightIn = new ArrayDeque<>();
+        rightOut = new ArrayDeque<>();
+        leftIn = new ArrayDeque<>();
+        leftOut = new ArrayDeque<>();
     }
 
     public void start() throws IOException {
-        System.out.println("Send or receive?");
-        String command = input.nextLine();
-        if (command.startsWith("s")) {
-            sendMode();
-        } else if (command.startsWith("r")) {
-            receiveMode();
+        System.out.println("Create or join?");
+        String command = keyboard.nextLine();
+        if (command.startsWith("c")) {
+            createGame();
+            gameLoop();
+            stop();
+        } else if (command.startsWith("j")) {
+            joinGame();
+            gameLoop();
+            stop();
         } else {
             System.out.println("Unknown command.");
+            stop();
         }
     }
 
     public void stop() throws IOException {
         serverSocket.close();
+        rightSocket.close();
+        leftSocket.close();
     }
 
-    private void sendMode() throws IOException {
-        System.out.println("Insert ip address to connect to:");
-        String ip = input.nextLine();
-        Socket clientSocket;
-        try {
-            clientSocket = new Socket(ip, PORT);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return;
-        }
-        
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        System.out.println("Connected. Insert message:");
-
-        String line = "";
-        while (!line.equals("quit")) {
-            System.out.println("Insert message:");
-            line = input.nextLine();
-            out.println(line);
-        }
-
-        in.close();
-        out.close();
-        clientSocket.close();
-    }
-
-    private void receiveMode() throws IOException {
+    public void createGame() throws IOException {
         serverSocket = new ServerSocket(PORT);
         System.out.println("Server socket hosted on " + InetAddress.getLocalHost() + ", port " + PORT);
-        
-        while (true) {
-            new ClientThread(serverSocket.accept()).start();
+        System.out.println("Waiting for right.");
+        rightSocket = serverSocket.accept();
+        rightHandler = new SocketHandler(rightSocket, rightIn, rightOut);
+        rightHandler.start();
+        rightOut.add("waitForConnection");
+        System.out.println("Right client connected from " + rightSocket.getInetAddress().getHostAddress());
+
+        System.out.println("Waiting for left.");
+        leftSocket = serverSocket.accept();
+        leftHandler = new SocketHandler(rightSocket, rightIn, rightOut);
+        leftHandler.start();
+        System.out.println("Left client connected from " + leftSocket.getInetAddress().getHostAddress());
+
+        System.out.println("Telling left to connect to right.");
+        leftOut.add("connectTo " + rightSocket.getInetAddress().getHostAddress());
+        while(leftIn.isEmpty()) {}
+
+        System.out.println("Left response: " + leftIn.poll());
+    }
+
+    public void joinGame() throws IOException {
+        System.out.print("Insert IP to connect to: ");
+        leftSocket = new Socket(keyboard.nextLine(), PORT);
+        leftHandler = new SocketHandler(leftSocket, leftIn, leftOut);
+        leftHandler.start();
+        System.out.println("Connected to left at " + leftSocket.getInetAddress().getHostAddress());
+        while(leftIn.isEmpty()) {}
+
+        String response = leftIn.poll();
+        if (response.startsWith("connectTo")) {
+            String rightIP = response.substring(9);
+            System.out.println("Connecting to " + rightIP);
+            rightSocket = new Socket(rightIP, PORT);
+            rightHandler = new SocketHandler(rightSocket, rightIn, rightOut);
+            rightHandler.start();
+            System.out.println("Connected to right at " + leftSocket.getInetAddress().getHostAddress());
+        } else if (response.equals("waitForConnection")) {
+            System.out.println("Waiting for right.");
+            rightSocket = serverSocket.accept();
+            rightHandler = new SocketHandler(rightSocket, rightIn, rightOut);
+            rightHandler.start();
+            System.out.println("Right client connected from " + rightSocket.getInetAddress().getHostAddress());
         }
     }
 
-    private static class ClientThread extends Thread {
-        private Socket socket;
-        private PrintWriter out;
-        private BufferedReader in;
-    
-        public ClientThread(Socket socket) {
-            this.socket = socket;
-        }
-    
-        public void run() {
-            try {
-                out = new PrintWriter(socket.getOutputStream(), true);
-                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                
-                String input;
-                while ((input = in.readLine()) != null) {
-                    System.out.println("got input: " + input);
-
-                    if (input.equals("quit")) {
-                        break;
-                    }
-                }
-        
-                in.close();
-                out.close();
-                socket.close();
-            } catch (Exception e) {
-                System.err.println(e.getMessage());
+    private void gameLoop() {
+        while (true) {
+            System.out.println("Gaming time started. Exit with 'exit'.");
+            String command = keyboard.nextLine();
+            if (command.equals("exit")) {
                 return;
             }
+
+            if (command.startsWith("left")) {
+                leftOut.add(command.substring(5));
+            } else if (command.startsWith("right")) {
+                rightOut.add(command.substring(5));
+            } 
         }
     }
 }
---
+++ SocketHandler.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayDeque;
+
+public class SocketHandler extends Thread {
+    private Socket socket;
+    private PrintWriter out;
+    private BufferedReader in;
+    private ArrayDeque<String> inQueue;
+    private ArrayDeque<String> outQueue;
+
+    public SocketHandler(Socket socket, ArrayDeque<String> inQueue, ArrayDeque<String> outQueue) {
+        this.socket = socket;
+        this.inQueue = inQueue;
+        this.outQueue = outQueue;
+    }
+
+    public void run() {
+        try {
+            out = new PrintWriter(socket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            
+            System.out.println("Client connected: " + socket.getInetAddress());
+
+            String input;
+            while (true) {
+                if (in.ready()) {
+                    input = in.readLine();
+
+                    if (input == null || input.equals("quit")) {
+                        break;
+                    }
+
+                    inQueue.add(input);
+                    System.out.println(socket.getInetAddress() + ": " + input);
+                }
+
+                while (!outQueue.isEmpty()) {
+                    out.println(outQueue.poll());
+                }
+            }
+    
+            System.out.println("Client disconnected: " + socket.getInetAddress());
+
+            in.close();
+            out.close();
+            socket.close();
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            return;
+        }
+    }
+}
